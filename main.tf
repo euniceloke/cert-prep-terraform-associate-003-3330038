9e74e709b780a7ca27c527e59e25a19d965cd379
Eunice Loke <eunice.loke@cenitex.vic.gov.au>
2024-12-20 15:42:36 +1100

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
-resource "aws_instance" "MyInstance_test" {
+resource "aws_instance" "ldap-ubuntu" {
     #instance configuration
+    ami = "ami-0d6560f3176dc9ec0"
+      instance_type = "t2.micro"
+
+      tags = {
+        Name = "ldap-ubuntu"
+      }
 }
